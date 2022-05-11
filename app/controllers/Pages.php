829ddcee5e54ad939c5414573b58d5eccf8719a4
Youngjae Lim <limyoungjae@gmail.com>
2022-05-11 14:29:39 -0400

    Add header footer layout

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -12,6 +12,10 @@
         }
 
         public function about() {
-            $this->view('pages/about');
+            $data = [
+                'title' => 'About Us'
+            ];
+
+            $this->view('pages/about', $data);
         }
     }
