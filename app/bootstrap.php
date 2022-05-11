473be52eb8bdf6014644df78095a643cfd4da813
Youngjae Lim <limyoungjae@gmail.com>
2022-05-11 11:41:58 -0400

    Add a config file and autoload libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
-    // Load libraries
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
+  // Load Config
+  require_once 'config/config.php';
+
+  // Autoload Core Libraries
+spl_autoload_register(
+    function ($className) {
+        include_once 'libraries/' . $className . '.php';
+    }
+);
