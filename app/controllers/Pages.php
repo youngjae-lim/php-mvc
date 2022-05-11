9218c912184f01702d14d2c148ff9a0ec3ff9a83
Youngjae Lim <limyoungjae@gmail.com>
2022-05-11 10:39:13 -0400

    Add a base Controller class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,10 +1,10 @@
 <?php
-    class Pages {
+    class Pages extends Controller {
         public function __construct() {
         }
 
         public function index() {
-
+            $this->view('hello');
         }
 
         public function about($id) {
