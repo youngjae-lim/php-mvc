da726f40280e99cd8629c17c51ab0f013a60736d
Youngjae Lim <limyoungjae@gmail.com>
2022-05-10 22:08:05 -0400

    Load the controller from the url

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -11,10 +11,32 @@
         protected $params = [];
 
         public function __construct() {
-            $this->getUrl();
+            // print_r($this->getUrl());
+
+            $url = $this->getUrl();
+
+            // Look in controllers for first value
+            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                // If exists, set as controller
+                $this->currentController = ucwords($url[0]);
+                // Unset 0 index
+                unset($url[0]);
+            }
+
+            // Require the controller
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            // Instaantiate controller class
+            $this->currentController = new $this->currentController;
+
         }
 
         public function getUrl() {
-            echo $_GET['url'];
+            if(isset($_GET['url'])) {
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
         }
     }
