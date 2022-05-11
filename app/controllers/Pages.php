a607440970f4310dfaa1df43aed58ed322b2a1b0
Youngjae Lim <limyoungjae@gmail.com>
2022-05-10 22:25:44 -0400

    Map methods and parameters

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,13 @@
 <?php
     class Pages {
         public function __construct() {
-            echo 'Pages loaded';
+        }
+
+        public function index() {
+
+        }
+
+        public function about($id) {
+            echo $id;
         }
     }
