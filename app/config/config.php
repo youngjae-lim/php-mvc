cff949b3407a085ed5a1576e537bbe06ab94d8dd
Youngjae Lim <limyoungjae@gmail.com>
2022-05-11 22:38:05 -0400

    Add Database class - part 1

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '123456');
+  define('DB_NAME', 'php_mvc');
+  
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
