0ce5693b4b06c440c731b4b33d9bac49e2b24555
Youngjae Lim <limyoungjae@gmail.com>
2022-05-12 00:16:55 -0400

    Add Database class - part 2

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,22 +1,31 @@
 <?php
-    class Pages extends Controller {
-        public function __construct() {
-          $this->postModel = $this->model('Post');
-        }
 
-        public function index() {
-            $data = [
-                'title' => 'Welcome'
-            ];
-
-            $this->view('pages/index', $data);
-        }
-
-        public function about() {
-            $data = [
-                'title' => 'About Us'
-            ];
-
-            $this->view('pages/about', $data);
-        }
+class Pages extends Controller
+{
+    public function __construct()
+    {
+        $this->postModel = $this->model('Post');
     }
+
+    public function index()
+    {
+        $posts = $this->postModel->getPosts();
+
+        $data = [
+            'title' => 'Welcome',
+            'posts' => $posts
+        ];
+
+
+        $this->view('pages/index', $data);
+    }
+
+    public function about()
+    {
+        $data = [
+            'title' => 'About Us'
+        ];
+
+        $this->view('pages/about', $data);
+    }
+}
--- app/libraries/database.php
+++ app/libraries/database.php
@@ -1,4 +1,5 @@
 <?php
+
   /*
    * PDO Database Class
    * Connect to database
@@ -6,7 +7,7 @@
    * Bind values
    * Return rows and results
    */
-  
+
 class Database
 {
     private $host = DB_HOST;
@@ -21,7 +22,7 @@ class Database
     public function __construct()
     {
         // Set DSN
-        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname; 
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
         $options = array(
           PDO::ATTR_PERSISTENT => true,
           PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
@@ -30,9 +31,64 @@ class Database
         // Create PDO instance
         try {
             $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             $this->error = $e->getMessage();
             echo $this->error;
         }
+    }
+
+    // Prepare statement with query
+    public function query($sql)
+    {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    // Bind values 
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    // Execute the prepared statement
+    public function execute()
+    {
+        return $this->stmt->execute();
+    }
+
+    // Get result set as array of objects
+    public function resultSet()
+    {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    // Get single record as object
+    public function single()
+    {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+
+    // Get row count
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
     }
 }
